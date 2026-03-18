00cd326ab7cdb772c7ea34fe0463bee0dad1e0cd
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-18 15:15:34 +0100

    Add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,12 @@
-
+const Navbar = ({ name }) => {
+  return (
+    <nav>
+      <h1 className="website__name">Fast Pizza Co.</h1>
+      <form className="nav__form">
+        <input type="text" placeholder="Search order #" maxLength={25} />
+      </form>
+      {name && <p className="user__name">Luka</p>}
+    </nav>
+  );
+};
+export default Navbar;
