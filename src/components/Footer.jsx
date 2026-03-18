66ec35463d1ddd3fcb1b88d081768f7c57d991ea
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-18 15:16:13 +0100

    Add Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,16 @@
-
+import { FaArrowRight } from "react-icons/fa6";
+const Footer = () => {
+  const pr = 27.0;
+  return (
+    <footer>
+      <div>
+        <p className="prodacts__number">2</p>
+        <p className="total__price">{pr.toFixed(2)}$</p>
+      </div>
+      <button type="button">
+        OPEN CART <FaArrowRight />
+      </button>
+    </footer>
+  );
+};
+export default Footer;
