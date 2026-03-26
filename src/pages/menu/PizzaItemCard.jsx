741e2d352282b7f710a8309b5f06ddf68024163f
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-26 22:25:34 +0100

    Add PizzaItemCard component

--- src/pages/menu/PizzaItemCard.jsx
+++ src/pages/menu/PizzaItemCard.jsx
@@ -1,1 +1,15 @@
-
+import Button from "../../components/Button";
+const PizzaItemCard = () => {
+  return (
+    <li className="pizza-list__item">
+      <img src="pizza.png" alt="pizza" className="pizza-img" />
+      <p className="pizza-name">Margherita</p>
+      <p className="pizza-ingredients">tomato, mozzarella, basil</p>
+      <p className="pizza-price">12$</p>
+      <Button btnType={"button"} variation={"primary-medium"}>
+        ADD TO CART
+      </Button>
+    </li>
+  );
+};
+export default PizzaItemCard;
