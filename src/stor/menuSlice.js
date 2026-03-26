5aeb70cd587a6ec1aad221d5e600d1187b8c1d1d
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-26 22:34:42 +0100

    feat: add menuSlice slice

---
+++ src/stor/menuSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchMenu } from "./api";
+const menuSlice = createSlice({
+  name: "menu",
+  initialState: {
+    menu: [],
+    status: "idle",
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMenu.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMenu.fulfilled, (state, action) => {
+        state.status = "success";
+        state.menu = action.payload;
+      })
+      .addCase(fetchMenu.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      });
+  },
+});
+export default menuSlice.reducer;
