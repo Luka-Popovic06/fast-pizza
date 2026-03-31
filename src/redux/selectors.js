5ba4096fe56ca147cc289ddc3ce5bde49df8a52b
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-31 23:06:12 +0200

    feat(selectors): add selector for total pizza quantity

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,3 +4,14 @@ export const selectPizzaQuantityByName = createSelector(
   [(state) => state.cart.pizzas, (state, pizzaName) => pizzaName],
   (pizzas, name) => pizzas[name]?.length,
 );
+export const selectTotalPizzasQuantity = createSelector(
+  (state) => state.cart.pizzas,
+  (pizzas) => {
+    let totalQuantity = 0;
+    for (const pizzaName in pizzas) {
+      const pizzaArray = pizzas[pizzaName];
+      totalQuantity += pizzaArray.length;
+    }
+    return totalQuantity;
+  },
+);
